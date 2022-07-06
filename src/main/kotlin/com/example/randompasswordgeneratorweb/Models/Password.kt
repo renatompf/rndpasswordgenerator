84d999f3ef984d1af14d3ecfe772f220b6b5ebc4
Renato Freire <renatompf@hotmail.com>
2022-07-06 07:52:13 +0100

    Add shuffle function and mandatory characters

--- src/main/kotlin/com/example/randompasswordgeneratorweb/Models/Password.kt
+++ src/main/kotlin/com/example/randompasswordgeneratorweb/Models/Password.kt
@@ -15,34 +15,62 @@ class Password(
     // Password's characters
     private val upperCaseLetters: CharArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()
     private val lowerCaseLetters: CharArray = "abcdefghijklmnopqrstuvwxyz".toCharArray()
-    private val specialCharacters: CharArray = "@#$%&({[]})<>".toCharArray()
+    private val specialCharacters: CharArray = "@#$€%&({[]})<>".toCharArray()
     private val numbers: CharArray = "1234567890".toCharArray()
 
-    fun generatePassword() : String{
-        if(length < 8)
+    private val random = SecureRandom.getInstance("SHA1PRNG")
+    fun shuffle(password: String) : String {
+        val passwordChars = mutableListOf<Char>()
+        for (c in password.toCharArray())
+            passwordChars.add(c)
+
+        val finalPassword = StringBuilder(password.length)
+        while (passwordChars.size != 0) {
+            finalPassword.append(passwordChars.removeAt(random.nextInt(passwordChars.size)))
+        }
+        return finalPassword.toString()
+    }
+
+    fun generatePassword(): String {
+        if (length < 8)
             throw InvalidLength();
 
-        val random = SecureRandom()
-        val password = StringBuilder()
-
+        val password = StringBuilder(length)
+        var passwordStartPosition : Int = 0
         val charCategoryToBeUsed = mutableListOf<CharArray>()
-        if(this.useUpper)
-            charCategoryToBeUsed.add(upperCaseLetters)
-        if(this.useLower)
-            charCategoryToBeUsed.add(lowerCaseLetters)
-        if(this.useNumbers)
-            charCategoryToBeUsed.add(numbers);
-        if(this.useSpecials)
-            charCategoryToBeUsed.add(specialCharacters);
 
-        if(charCategoryToBeUsed.isEmpty())
+        if (this.useUpper) {
+            charCategoryToBeUsed.add(upperCaseLetters)
+            password.append(upperCaseLetters.get(random.nextInt(upperCaseLetters.size)))
+            passwordStartPosition +=1
+        }
+
+        if (this.useLower) {
+            charCategoryToBeUsed.add(lowerCaseLetters)
+            password.append(lowerCaseLetters.get(random.nextInt(lowerCaseLetters.size)))
+            passwordStartPosition +=1
+        }
+
+        if (this.useNumbers) {
+            charCategoryToBeUsed.add(numbers)
+            password.append(numbers.get(random.nextInt(numbers.size)))
+            passwordStartPosition +=1
+        }
+
+        if (this.useSpecials) {
+            charCategoryToBeUsed.add(specialCharacters)
+            password.append(specialCharacters.get(random.nextInt(specialCharacters.size)))
+            passwordStartPosition +=1
+        }
+
+        if (charCategoryToBeUsed.isEmpty())
             throw InvalidNumberOfParameters()
 
-        for( i in 0..length-1){
-            val charCategory : CharArray = charCategoryToBeUsed.get(random.nextInt(charCategoryToBeUsed.size))
+        for (i in passwordStartPosition until length) {
+            val charCategory: CharArray = charCategoryToBeUsed.get(random.nextInt(charCategoryToBeUsed.size))
             password.append(charCategory[random.nextInt(charCategory.size)])
         }
 
-        return String(password)
+        return shuffle(password.toString())
     }
 }
